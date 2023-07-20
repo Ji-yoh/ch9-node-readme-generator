b014d289461f73cf6d7b77cb3d5ac621f97d56ad
Ji-yoh <geovannih514@gmail.com>
2023-07-20 15:35:13 -0700

    moved license array out of inq prompts, renderBadges function should work, need license links

--- Develop/index.js
+++ Develop/index.js
@@ -3,6 +3,7 @@ const inquirer = require("inquirer");
 const fs = require("fs")
 const generateMarkdown = require("./utils/generateMarkdown.js")
 
+
 // TODO: Create an array of questions for user input
 
 const questions = [
@@ -17,11 +18,30 @@ const questions = [
     'What is your GitHub username?'
 ];
 
+// originally had this array included in prompts but moved outside for clarity and to use in generateMarkdown.js
+// add list from Github
+const license = [
+    'Apache 2.0', 
+    'GNU GPL v3',
+    'BSD 2',
+    'BSD 3', 
+    'Boost', 
+    'Creative Commons Zero v1', 
+    'Eclipse Public 2.0', 
+    'GNU Alfero v3.0', 
+    'GNU General 2.0', 
+    'GNU Lesser General 2.1', 
+    'Mozilla Public 2.0', 
+    'MIT', 
+    'The Unlicense', 
+    'None'
+]
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) => {
-
+        err ? console.log(err) : console.log('README.md created successfully!')
     })
 }
 
@@ -54,7 +74,7 @@ function init() {
             type: 'list',
             name: 'license',
             message: questions[4],
-            choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None']
+            choices:  license 
         },
         {
             type: 'input',
@@ -78,7 +98,7 @@ function init() {
         }
     ]).then((responses) => {
         console.log(responses)
-        writeToFile('README.md', generateMarkdown(responses));
+        writeToFile('./output/README.md', generateMarkdown(responses));
     }) 
 }
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,21 +1,47 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// license badge from Shields.io: https://img.shields.io/badge/any_text-you_like-blue
+// replace any_text with license name
+function renderLicenseBadge(license) {
+  const licenseBadgeParam = license.replace(/ /g, '_'); // replace spaces with underscores for URL
+
+  switch (license) {
+    case 'None':
+      return '';
+    case 'Apache 2.0':
+    case 'GNU GPL v3':
+    case 'BSD 2':
+    case 'BSD 3': 
+    case 'Boost': 
+    case 'Creative Commons Zero v1':
+    case 'Eclipse Public 2.0':
+    case 'GNU Alfero v3.0':
+    case 'GNU General 2.0': 
+    case 'GNU Lesser General 2.1':
+    case 'Mozilla Public 2.0': 
+    case 'MIT':
+    case 'The Unlicense':
+      return `https://img.shields.io/badge/${licenseBadgeParam}-blue` // badge URL
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
+// license link: https://choosealicense.com/licenses/
 function renderLicenseLink(license) {}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(responses) {
-  return `# ${responses.title}
-  ## ${responses.description}
-  ## ${responses.contents}
 
+function generateMarkdown(data) {
+  return `# ${data.title}
+  ## ${data.description}
+  ## ${data.contents}
+  
 `;
 }
 
